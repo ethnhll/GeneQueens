0b6814f46a6a5fe9a6f0c73df5286e3e99519f32
Ethan Hill <hill.1303@gmail.com>
2013-10-28 16:45:10 -0400

    Major additions, created Node Class and filled out several functions

--- src/main/java/edu/ohio_state/cse/genequeens/Node.java
+++ src/main/java/edu/ohio_state/cse/genequeens/Node.java
@@ -1,6 +1,100 @@
 package edu.ohio_state.cse.genequeens;
 
+import java.util.Arrays;
+
+/**
+ * 
+ * @author Ethan
+ *
+ */
 public class Node {
+
+	/**
+	 * 
+	 */
+	private final int score;
+	
+	/**
+	 * 
+	 */
+	private final int[] state;
+	
+	/**
+	 * 
+	 * @param score
+	 * @param state
+	 */
+	public Node(int score, int[] state) {
+		this.score = score;
+		this.state = state;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getScore(){
+		return this.score;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int[] getState(){
+		return this.state;
+	}
+
+
+	@Override
+	public int hashCode() {
+		
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + score;
+		result = prime * result + Arrays.hashCode(state);
+		return result;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		boolean equal = true;
+		
+		if (this == obj) {
+			equal = true;
+		}
+		if (obj == null) {
+			equal = false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			equal = false;
+		}
+		
+		Node otherNode = (Node) obj;
+		
+		if (this.score != otherNode.getScore()) {
+			equal = false;
+		}
+		if (!Arrays.equals(this.state, otherNode.getState())) {
+			equal = false;
+		}
+		
+		return equal;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("(");
+		builder.append(this.score);
+		builder.append(", ");
+		builder.append(Arrays.toString(this.state));
+		builder.append(")");
+		return builder.toString();
+	}
 	
 	
 
--- src/main/java/edu/ohio_state/cse/genequeens/TestHarness.java
+++ src/main/java/edu/ohio_state/cse/genequeens/TestHarness.java
@@ -1,6 +1,8 @@
 package edu.ohio_state.cse.genequeens;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class TestHarness {
@@ -51,6 +53,7 @@ public class TestHarness {
 
 				float slope = ((float) rowA - rowB)
 						/ ((float) columnA - columnB);
+
 				if ((Math.abs(slope) == 1F) || slope == 0F) {
 					/*
 					 * If the set already contains an element, then the set
@@ -61,31 +64,74 @@ public class TestHarness {
 				}
 			}
 		}
-		int safeQueens = board.length - attackingQueens.size();
+		int safeQueens =  attackingQueens.size();
 		return safeQueens;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	private Node successors(int[] state){
-		
-		
+
+	/**
+	 * 
+	 * @param state
+	 * @return
+	 */
+	private static Set<Node> successors(int[] state) {
+
+		Set<Node> successors = new HashSet<Node>();
+
+		/*
+		 * Add the parent state to the set of successors to remove after all
+		 * successors are generated. This is to ensure that if a possible
+		 * successor matches the parent state, that it is not added to the set.
+		 */
+		int parentScore = boardScore(state);
+		Node parent = new Node(parentScore, state);
+		successors.add(parent);
+
+		for (int columnIndex = 0; columnIndex < state.length; columnIndex++) {
+
+			int[] tempState = Arrays.copyOf(state, state.length);
+
+			for (int rowVal = 0; rowVal < state.length; rowVal++) {
+
+				tempState[columnIndex] = rowVal;
+				int stateScore = boardScore(tempState);
+				Node child = new Node(stateScore, tempState);
+
+				successors.add(child);
+			}
+		}
+		successors.remove(parent);
+		return successors;
 	}
+
 	
-	public int[] hillClimbingAgent(int boardsize){
+	public int[] hillClimbingAgent(int boardSize) {
 		
+		int[] initialState = new int[boardSize];
+		
+		//Generate random numbers to fill an array of length boardSize
+		for(int columnIndex = 0; columnIndex < initialState.length; columnIndex++){
+			//Random number between 0 and boardSize-1
+			Random rand = new Random();
+			//Implicitly, rand.nextInt((MAX(=boardSize-1)-MIN(=0))+1) + MIN(=0);
+			int randomVal = rand.nextInt(boardSize);
+			initialState[columnIndex] = randomVal;
+		}
+		
+		return null;
 	}
 
 	public static void main(String[] args) {
 
-		int[] board = { 2, 1, 3, 3 };
+		int[] board = { 1, 0, 1};
 
-		System.out.println(boardScore(board));
+		Set<Node> nodes = successors(board);
+		
+		for (Node node : nodes){
+			System.out.println(node.toString());
+		}
+		
+		
+		
 
 	}
 
